d6ba3f1440b81df44d104129a0db5ce8632adc04
sofi.spag <sofi.spag@gmail.com>
2019-07-11 23:10:26 -0700

    creating database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double bacon cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
