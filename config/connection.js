beae046f0dece15f820e4adc0b28b6e469bb3d33
sofi.spag <sofi.spag@gmail.com>
2019-07-12 20:13:30 -0700

    reverting credentials stuff

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
 var mysql = require("mysql");
 var connection;
 
-
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  console.log("DATABASE INIT");
-  console.log(JSON.stringify(process.env));
   connection = mysql.createConnection({
-    host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "pgs8o25el39wzfa6",
-  password: "wzm8ghiqwyxg5ilw",
-  database: "ozu8dsl1g5unxcjk"
+    host: "localhost",
+  user: "root",
+  password: "123123123",
+  database: "burgers_db"
   });
 }
 
