07398cbacd03b590c3739298f5b613a94a6ff3fd
sofi.spag <sofi.spag@gmail.com>
2019-07-12 20:01:09 -0700

    extra logging

--- config/connection.js
+++ config/connection.js
@@ -7,15 +7,21 @@ if(process.env.JAWSDB_URL){
   console.log("DATABASE INIT - JAWS");
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  console.log("DATABASE INIT - NO JAWS");
+  console.log("DATABASE INIT - " + process.env);
+  console.log("DATABASE INIT - CREATING CONNECTION");
   connection = mysql.createConnection({
     host: "localhost",
   user: "root",
   password: "123123123",
   database: "burgers_db"
   });
+  console.log("DATABASE INIT - CONNECTION FINISHED");
 }
 
+console.log("DATABASE INIT - TRY TO CONNECT");
 connection.connect();
+console.log("DATABASE INIT - CONNECT FINISHED");
 
 // var connection = mysql.createConnection({
 //   host: "localhost",
